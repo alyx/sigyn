e53123f728b8be9c6e2d5f60d9d36d288cb6b1f6
Alyx <alyx@sporksmoo.net>
2011-06-23 16:59:46 -0500

    Embed an irc_user_t here, as it makes more sense to give ourself a user.

--- include/sigyn.h
+++ include/sigyn.h
@@ -11,20 +11,16 @@
 #include <unistd.h>
 #include <mowgli.h>
 #include "logger.h"
+#include "irc.h"
 #include "config.h"
 
 struct me {
-    char *nick = SIGYA_NICK;
+    irc_user_t *client;
     char *channels[];
     struct uplink {
         int sock;
-        int port = UPLINK_PORT;
-        char *hostname = UPLINK_SERVER;
-        struct addrinfo hints {
-            int ai_family = AF_UNSPEC;
-            int ai_socktype = SOCK_STREAM;
-        };
-        struct addrinfo *res;
+        int port;
+        char *hostname;
     };
     struct stats {
         struct time {
@@ -34,22 +30,5 @@ struct me {
         int outB = 0;
     };
 } me;
-
-int raw(char *line, ...) {
-    char *sendbuf[510];
-    va_list args;
-    
-    va_start(args, line);
-    vsnprintf(sendbuf, 509, line, args);
-    va_end(args);
-
-    strlcpy(sendbuf, "\r\n");
-
-    int sent = send(socket, sendbuf, strlen(sendbuf), 0);
-    me.stats.outB += sent;
-    char *logout = snprintf("<< %s", sendbuf);
-    log(LOG_RAW, logout);
-    return sent;
-}
 
 void parse(char *text);
