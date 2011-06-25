a9cebe30881c3d1415d0dec902b992633454b4ea
Alyx <alyx@sporksmoo.net>
2011-06-24 20:26:17 -0500

    Added IRC commands according to RFC 1459.

--- src/irc.c
+++ src/irc.c
@@ -6,7 +6,7 @@
 int raw(char *line, ...) {
     char *sendbuf[510];
     va_list args;
-    
+
     va_start(args, line);
     vsnprintf(sendbuf, 509, line, args);
     va_end(args);
@@ -20,39 +20,372 @@ int raw(char *line, ...) {
     return sent;
 }
 
-void irc_nick(char *newnick)
+static void serv_optional(char *server, char *command)
 {
-    raw("NICK %s", newnick);
+    /* Many commands can take a sole
+     * optional servername as a param.
+     * This function simplifies the process.
+     */
+    if (server != NULL)
+        raw("%s %s", command, server);
+    else
+        raw("%s", command);
+}
+
+/* RFC 1459 commands follow.
+ * RFC 1459 is the original
+ * IRC RFC by J. Oikarinen.
+ * http://www.ietf.org/rfc/rfc1459.txt
+ */
+
+/* 4.1: Connection registration. */
+
+void irc_pass(char *password)
+{
+    /* 4.1.1: PASS
+     * Parameters: <password>
+     * Example: PASS secretpasswordhere
+     */
+    raw("PASS %s", password);
+}
+
+void irc_nick(char *nick)
+{
+    /* 4.1.2: NICK
+     * Parameters: <nickname> [ <hopcount> ]
+     * Example: NICK Wiz
+     */
+    raw("NICK %s", nick);
 }
 
 void irc_user(char *user, char *host, char *server, char *real)
 {
+    /* 4.1.3: USER
+     * Parameters: <username> <hostname> <servername> <realname>
+     * Example: USER guest tolmoon tolsun :Ronnie Reagan
+     */
     raw("USER %s %s %s :%s", user, host, server, real);
 }
 
-void irc_pass(char *password)
+void irc_oper(char *user, char *password)
 {
-    raw("PASS %s", password);
+    /* 4.1.5: OPER
+     * Parameters: <user> <password>
+     * Example: OPER foo bar
+     */
+    raw("OPER %s %s", user password);
 }
 
 void irc_quit(char *message)
 {
+    /* 4.1.6: QUIT
+     * Parameters: [<Quit message>]
+     * Example: QUIT :Gone to have lunch
+     */
     raw("QUIT :%s", message);
 }
 
-void irc_pong(char *message)
+void irc_squit(char *server, char *message)
 {
-    raw("PONG :%s", message);
+    /* 4.1.7: SQUIT
+     * Parameters: <server> <comment>
+     * Example: SQUIT tolsun.oulu.fi :Bad Link
+     */
+    raw("SQUIT %s :%s", server, message);
 }
 
-void irc_privmsg(char *target, char *message)
+/* 4.2: Channel operations */
+
+void irc_join(char *channel, char *key)
 {
-    raw("PRIVMSG %s :%s", target, message);
+    /* 4.2.1: JOIN
+     * Parameters: <channel>{,<channel>} [<key>{,<key>}]
+     * Example: JOIN #foo,#bar fubar,foobar 
+     */
+    if (key != NULL)
+        raw("JOIN %s %s", channel, key);
+    else
+        raw("JOIN %s", channel);
 }
 
-void irc_notice(char *target, char *message)
+void irc_part(char *channel)
 {
-    raw("NOTICE %s: %s", target, message);
+    /* 4.2.2: PART
+     * Parameters: <channel>{,<channel>}
+     * Example: PART #twilight_zone
+     */
+    raw("PART %s", channel);
 }
 
+void irc_mode(char *target, char *modestring)
+{
+    /* 4.2.3: MODE
+     *  4.2.3.1: Channel modes
+     *    Parameters: <channel> {[+|-]|o|p|s|i|t|n|b|v} [<limit>] [<user>] [<ban mask>]
+     *    Example: MODE #Finnish +im
+     *  4.2.3.2: User modes
+     *    Parameters: <nickname> {[+|-]|i|w|s|o}
+     *    Example: MODE WiZ -o
+     */
+    /* XXX: Stop being lazy and code a real mode command. */
+    raw("MODE %s %s", target, modestring);
+}
+
+void irc_topic(char *channel, char *topic)
+{
+    /* 4.2.4: TOPIC
+     * Parameters: <channel> [<topic>]
+     * TOPIC #test :another topic
+     */
+    if (topic != NULL)
+        raw("TOPIC %s :%s", channel, topic);
+    else
+        raw("TOPIC %s", channel);
+}
+
+void irc_names(char *channel)
+{
+    /* 4.2.5: NAMES
+     * Parameters: [<channel>{,<channel>}]
+     * Example: NAMES #twilight_zone,#42
+     */
+    raw("NAMES %s", channel);
+}
+
+void irc_invite(char *user, char *channel)
+{
+    /* 4.2.7: INVITE
+     * Parameters: <nickname> <channel>
+     * Example: INVITE WiZ #Twilight_Zone
+     */
+    raw("INVITE %s %s", user, channel);
+}
+
+void irc_kick(char *channel, char *user, char *comment)
+{
+    /* 4.2.8: KICK
+     * Parameters: <channel> <user> [<comment>]
+     * Example: KICK #Finnish John :Speaking English
+     */
+    if (comment != NULL)
+        raw("KICK %s %s :%s", channel, user, comment);
+    else
+        raw("KICK %s %s", channel, user);
+}
+
+/* 4.3: Server queries and commands */
+
+void irc_version(char *server)
+{
+    /* 4.3.1: VERSION
+     * Parameters: [<server>]
+     * Example: VERSION tolsun.oulu.fi 
+     */
+    if (server != NULL)
+        raw("VERSION %s", server);
+    else
+        raw("VERSION");
+}
+
+void irc_stats(char *query, char *server)
+{
+    /* 4.3.2: STATS
+     * Parameters: [<query> [<server>]]
+     * Example: STATS c eff.org
+     */
+    if (server != NULL)
+        raw("STATS %s %s", query, server);
+    else
+        raw("STATS %s", query);
+}
+
+void irc_links(char *remote, char *mask)
+{
+    /* 4.3.3: LINKS
+     * Parameters: [[<remote server>] <server mask>]
+     * LINKS *.bu.edu *.edu
+     */
+    if (remote != NULL && mask != NULL)
+        raw("LINKS %s %s", remote, mask);
+    else if (remote != NULL && mask == NULL)
+        raw("LINKS %s", remote);
+    else if (remote == NULL)
+        raw("LINKS");
+}
+
+void irc_time(char *server)
+{
+    /* 4.3.4: TIME
+     * Parameters: [<server>]
+     * Example: TIME tolsun.oulu.fi  
+     */
+    serv_optional(server, "TIME");
+}
+
+void irc_connect(char *target, int port, char *remote)
+{
+    /* 4.3.5: CONNECT
+     * Parameters: <target server> [<port> [<remote server>]]
+     * Example: CONNECT tolsun.oulu.fi
+     */
+    if (remote != NULL && port != NULL)
+        raw("CONNECT %s %d %s", target, port, remote);
+    else if (remote != NULL && port == NULL)
+        raw("CONNECT %s %s", target, remote);
+    else
+        raw("CONNECT %s", target);
+}
+
+void irc_trace(char *server)
+{
+    /* 4.3.6: TRACE
+     * Parameters: [<server>]
+     * Example: TRACE *.oulu.fi
+     */
+    serv_optional(server, "TRACE");
+}
+
+void irc_admin(char *server)
+{
+    /* 4.3.7: ADMIN
+     * Parameters: [<server>]
+     * Example: ADMIN tolsun.oulu.fi
+     */
+    serv_optional(server, "ADMIN");
+}
+
+void irc_info(char *server)
+{
+    /* 4.3.8: INFO
+     * Parameters: [<server>]
+     * Example: INFO csd.bu.edu
+     */
+    serv_optional(server, "INFO");
+}
+
+/* 4.4: Sending messages */
+
+void irc_privmsg(char *receiver, char *text)
+{
+    /* 4.4.1: PRIVMSG
+     * Parameters: <receiver>{,<receiver>} <text to be sent>
+     * Example: PRIVMSG Angel :yes I'm receiving it!
+     */
+    raw("PRIVMSG %s :%s", receiver, text);
+}
+
+void irc_notice(char *nickname, char *text)
+{
+    /* 4.4.2: NOTICE
+     * Parameters: <nickname> <text>
+     * Example: NOTICE Angel :Foobar
+     */
+    raw("NOTICE %s :%s", nickname, text);
+}
+
+/* 4.5: User based queries */
+
+void irc_who(char *name, char *o)
+{
+    /* 4.5.1: WHO
+     * Parameters: [<name> [<o>]]
+     * Example: WHO *.fi
+     */
+    if (o != NULL)
+        raw("WHO %s %s", name, o);
+    else
+        raw("WHO %s", name);
+}
+
+void irc_whois(char *server, char *nickmask)
+{
+    /* 4.5.2: WHOIS
+     * Parameters: [<server>] <nickmask>[,<nickmask>[,...]]
+     * Example: WHOIS eff.org trillian
+     */
+    if (server != NULL)
+        raw("WHOIS %s %s", server, nickmask);
+    else
+        raw("WHOIS %s", nickmask);
+}
+
+void irc_whowas(char *nickname, int count, char *server)
+{
+    /* 4.5.3: WHOWAS
+     * Parameters: <nickname> [<count> [<server>]]
+     * WHOWAS Trillian 1 *.edu
+     */
+    if (count != NULL && server != NULL)
+        raw("WHOWAS %s %d %s", nickname, count, server);
+    else if (count != NULL && server == NULL)
+        raw("WHOWAS %s %d", nickname, count);
+    else if (count == NULL && server != NULL)
+        raw("WHOWAS %s %s", nickname, server);
+    else
+        raw("WHOWAS %s", nickname);
+}
+
+/* 4.6: Miscellaneous messages */
+
+void irc_kill(char *nickname, char *comment)
+{
+    /* 4.6.1: KILL
+     * Parameters: <nickname> <comment>
+     * KILL Foo :Random reason
+     */
+    raw("KILL %s :%s", nickname, comment);
+}
+
+void irc_ping(char *server1, char *server2)
+{
+    /* 4.6.2: PING
+     * Parameters: <server1> [<server2>]
+     * PING tolsun.oulu.fi 
+     */
+    if (server2 != NULL)
+        raw("PING %s %s", server1, server2);
+    else
+        raw("PING %s", server1);
+}
+
+void irc_pong(char *daemon)
+{
+    /* 4.6.3: PONG
+     * Parameters: <daemon> [<daemon2>]
+     * PONG tolsun.oulu.fi
+     */
+    raw("PONG %s", daemon);
+}
+
+/* 5: Optionals */
+
+void irc_away(char *message)
+{
+    /* 5.1: AWAY
+     * Parameters: [message]
+     * Example: AWAY :Gone to lunch.  Back in 5
+     */
+    if (message != NULL)
+        raw("AWAY :%s", message);
+    else
+        raw("AWAY");
+}
+
+void irc_users(char *server)
+{
+    /* 5.5: USERS
+     * Parameters: [<server>]
+     * Example: USERS eff.org
+     */
+    serv_optional(server, "USERS");
+}
+
+void irc_userhost(char *nickname)
+{
+    /* 5.7: USERHOST
+     * Parameters: <nickname>{<space><nickname>}
+     * USERHOST Wiz Michael Marty p
+     */
+    raw("USERHOST %s", nickname);
+}
 
