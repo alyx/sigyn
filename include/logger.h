24e2e1eb2ce3fe189a6258b5f0462041583f73ee
Alyx <alyx@sporksmoo.net>
2011-06-26 16:39:58 -0500

    Make the logger really sexy

--- include/logger.h
+++ include/logger.h
@@ -10,6 +10,7 @@
 #include "config.h"
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  * define various log levels;
@@ -25,22 +26,35 @@ enum log_levels {
 
 FILE *logfile;
 
-void logger(int level, char *message, ...)
+void logger_init(const char *filename)
 {
-    if ((logfile = fopen(LOGFILE, "a")) == NULL)
+    if ((logfile = fopen(filename, "a")) == NULL)
     {
         fprintf(stderr, "Cannot open logfile\n");
         exit(1);
     }
-    if (level >= LOG_LEVEL)
-    {
-        char logline[1024];
-        va_list args;
-        va_start(args, message);
-        vsnprintf(logline, 1024, message, args);
-        va_end(args);
-        /*strlcpy(logline, "\n");*/
-        strncat(logline, "\n", 1);
-        fprintf(logfile, logline);
-    }
+}
+
+static void va_logger(unsigned int level, const char *format, va_list args)
+{
+    char buf[BUFSIZE];
+    char datetime[64];
+    time_t t;
+    struct tm tm;
+
+    vsnprintf(buf, BUFSIZE, format, args);
+
+    time(&t);
+    tm = *localtime(&t);
+    strftime(datetime, sizeof(datetime) - 1, "[%d/%m/%Y %H:%M:%S]", &tm);
+
+    fprintf(logfile, "%s %s\n", datetime, buf);
+}
+
+void logger(unsigned int level, char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    va_logger(level, format, args);
+    va_end(args);
 }
