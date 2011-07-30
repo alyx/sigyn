93bbc605276b24ba36ba236fa27cc22ea562a199
Alyx Wolcott <alyx@malkier.net>
2011-07-30 19:43:31 +0000

    initialise the logger

--- tests/parse_self.c
+++ tests/parse_self.c
@@ -2,13 +2,15 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/types.h>
-/*#include <libmowgli/mowgli.h>*/
+#include <libmowgli/mowgli.h>
+#include "logger.h"
 #include "parser.h"
 #include "test.h"
 
 int main(void)
 {
     irc_event_t *out;
+    logger_init("sigyn.log");
 
     out = parse(":NOTsvchost!~none@c-68-58-12-123.hsd1.il.comcast.net PRIVMSG #malkier :even though i dont buy tyhe notion that bachelor degree means almost nothing anymore, i do believe that a masters sets you apart");
     printf("User: %s!%s@%s\nCommand: %s\nParams: %s\n", out->origin->nick,
