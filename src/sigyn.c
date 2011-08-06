0fe7ca940611d40ea6e6a9a68c39e4a4d7bf60e7
Alyx <alyx@malkier.net>
2011-08-05 19:20:40 -0700

    Replace closer differences with a replacement from platform.c

--- src/sigyn.c
+++ src/sigyn.c
@@ -22,18 +22,13 @@ int main(int argc, char *argv[])
     initialise_sigyn(SIGYN_NICK, SIGYN_NICK, SIGYN_REALNAME, UPLINK_SERVER, UPLINK_PORT);
     uplink_connect(me.uplink.hostname, me.uplink.port);
 
-    char *text = mowgli_alloc(513);
+    char text[513];
     irc_event_t *event = mowgli_alloc(sizeof(irc_event_t));
-    /*irc_event_t *event = malloc(sizeof(irc_event_t));*/
     while (1)
     {
-        fread(text, 1, 512, sock);
+        fread(text, 1, 512, me.uplink.sock);
         event = parse(text);
     }
-#ifdef _WIN32
-    closesocket(me.uplink.sock);
-#else
-    close(me.uplink.sock);
-#endif
+    close_socket(me.uplink.sock);
     return 0;
 }
