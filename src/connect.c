c4d50762357fa411f00c700bdf01d940474105d4
Alyx <alyx@malkier.net>
2011-08-05 19:19:12 -0700

    Getting closer to win32 support (Maybe.)!

--- src/connect.c
+++ src/connect.c
@@ -1,75 +1,74 @@
 /* Sigyn - A reasonably sane IRC bot.
- * Copyright (c) Alexandria Wolcott <alyx@malkier.net>
+ * Copyright (c) 2011 Alyx Wolcott <alyx@malkier.net>
  * Released under the BSD license.
+ *
+ * Abstract: This module handles connecting to the IRC server.
  */
 
 #include "sigyn.h"
 
 #ifdef _WIN32
+    static const int wsres;
     WSADATA wsaData;
 #endif
 
 void uplink_connect(char *uplink, int *port)
 {
+    char *hostname = sigyn_hostname();
     struct addrinfo *res, hints;
     hints = { 0 };
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
+
 #ifdef _WIN32
-    int wsres;
     wsres = WSAStartup(MAKEWORD(2,2), &wsaData);
     switch(wsres)
     {
         case 0:
-            logger(LOG_DEBUG, "Successfully started Winsock.");
+            logger(LOG_STATUS, "Successfully started Winsock.");
         case WSASYSNOTREADY:
-            logger(LOG_WARNING, "Cannot start winsock: Underlying network is not ready for communication.");
-            exit(0);
+            fatal("Cannot start winsock: Underlying network is not ready for communication.");
         case WSAVERNOTSUPPORTED:
             /* Note: This should never happen. */
-            logger(LOG_WARNING, "Cannot start winsock: This version of winsock is not supported by this winsock implementation.");
-            exit(0);
+            fatal("Cannot start winsock: This version of winsock is not supported by this winsock implementation.");
         case WSAEPROCLIM:
-            logger(LOG_WARNING, "Cannot start winsock: Winsock task limit reached.")
-            exit(0);
+            fatal("Cannot start winsock: Winsock task limit reached.")
         default:
-            logger(LOG_WARNING, "Cannot start winsock: Unknown errno %d", wsres);
-            exit(0);
+            fatal("Cannot start winsock: Unknown errno %d", wsres);
     }
+#endif
 
-#else
-    /*char *hostname = mowgli_alloc(256);*/
-    char *hostname = malloc(256);
-    gethostname(hostname, 255);
-
-    switch( getaddrinfo( uplink, port, &hints, &res) )
+    int result;
+    result = getaddrinfo( uplink, port, &hints, &res);
+    switch(result)
     {
+        case 0:
+            logger(LOG_STATUS, "Hostname resolution successful");
         case EAI_AGAIN:
-            logger(LOG_WARNING, "Cannot resolve hostname: The name server returned a temporary failure indication. Try again later.");
-            exit(0);
+            fatal("Cannot resolve hostname: The name server returned a temporary failure indication. Try again later.");
         case EAI_FAIL:
-            logger(LOG_WARNING, "Cannot resolve hostname: The name server returned a permanent failure indication.");
-            exit(0);
+            fatal("Cannot resolve hostname: The name server returned a permanent failure indication.");
         case EAI_MEMORY:
-            logger(LOG_WARNING, "Cannot resolve hostname: Out of memory.");
-            exit(0);
+            fatal("Cannot resolve hostname: Out of memory.");
         case EAI_SYSTEM:
-            logger(LOG_WARNING, "Cannot resolve hostname: System returned error: %i", errno);
-            exit(0);
+            fatal("Cannot resolve hostname: System returned error: %i", ERRNO);
+        default:
+            char tmp[40+sizeof(result)];
+            fatal("Cannot resolve hostname: Unknown error %d", result);
     }
 
     logger(LOG_STATUS, "Attempting to connect to %s:%d", uplink, port);
-    me.uplink.sock = socket(res.ai_family, res.ai_socktype, res.ai_protocol);
+    me.uplink.sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
     if( connect(me.uplink.sock, res.ai_addr, res.ai_addrlen) != -1)
         logger(LOG_STATUS, "Connection to %s:%d successful", uplink, port);
     else
     {
-        logger(LOG_STATUS, "Connection to %s:%d failed: %i", uplink, port, errno);
-        exit(0);
+        logger(LOG_STATUS, "Connection to %s:%d failed: %i", uplink, port, ERRNO);
+        fatal("Connection failed");
     }
-#endif
+
     if(UPLINK_PASS)
         irc_pass(UPLINK_PASS);
     irc_nick(me.nick);
