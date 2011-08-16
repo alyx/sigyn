2ef4d101298900f4141d711bbc239d9302a164d5
Alyx Wolcott <alyx@malkier.net>
2011-08-16 03:52:03 +0000

    Oops, forgot to add this in platform changes

--- include/sigyn.h
+++ include/sigyn.h
@@ -17,10 +17,9 @@
 #include "platform.h"
 #include "logger.h"
 #include "irc.h"
-#include "config.h"
+#include "sigyn_config.h"
 
 extern irc_event_t *parse(char *text);
-extern void uplink_connect(char *uplink, int *port);
+extern void uplink_connect(char *uplink, int port);
 extern int sigyn_hostname(char *host, int len);
-extern void sigyn_fatal(char *format, ...);
 #endif
