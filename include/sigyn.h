390aa9aecf88cc69901e14b9115e1b6dd4520724
Alyx <alyx@malkier.net>
2011-09-18 18:38:28 -0500

    Moved the random function defs to tools.h, added some limits here, added the minini header, new sigyn command header, et cetera.

--- include/sigyn.h
+++ include/sigyn.h
@@ -15,8 +15,13 @@
 #include <unistd.h>
 #include <libmowgli/mowgli.h>
 
-#define HOSTLEN 64
+/* Define some limits. */
+#define BUFSIZE 1024
+#define MAXPARC 20
+
+#include "minini/minini.h"
 #include "platform.h"
+#include "command.h"
 #include "irc.h"
 #include "ircstream.h"
 #include "logger.h"
@@ -24,24 +29,7 @@
 #include "module.h"
 #include "queue.h"
 #include "sigyn_config.h"
-
-/* Define functions from string.c */
-
-extern void strip(char *line, char *strippers); 
-#ifndef HAVE_STRLCAT
-extern size_t strlcat(char *dest, const char *src, size_t count);
-#endif
-#ifndef HAVE_STRLCPY
-extern size_t strlcpy(char *dest, const char *src, size_t count);
-#endif
-
-extern irc_event_t *parse(char line[]);
-extern void preparse(char line[]);
-extern void sigyn_fatal(char *format, ...);
-void sigyn_cleanup(void);
-extern int StartWSA(void);
-extern int sigyn_hostname(char *host, int len);
-extern socket_t uplink_connect(char *uplink, int port, char *vhost);
-extern void uplink_disconnect(void);
+#include "tools.h"
+#include "version.h"
 
 #endif
