acc8f2d7ea5c19d0be94d66c3c1f1f94b6545ed8
Alyx Wolcott <alyx@malkier.net>
2011-10-15 01:43:40 +0000

    Hello has a timer example!

--- modules/example/hello.c
+++ modules/example/hello.c
@@ -5,17 +5,20 @@ DECLARE_MODULE("examples/hello", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddein
 
 static void handle_privmsg(void *data, UNUSED void *udata);
 static void cmd_hello(irc_event_t *event, UNUSED int parc, UNUSED char **parv);
+static bool timed_cmd_hello(UNUSED void *argument);
 
 void _modinit(UNUSED module_t *m)
 {
     mowgli_hook_associate("PRIVMSG", handle_privmsg, NULL);
     command_add("hello", cmd_hello, "says hello to you!", "<user>");
+    timer_add("hello", timed_cmd_hello, NULL, 10);
 }
 
 void _moddeinit(UNUSED module_unload_intent_t intent)
 {
     mowgli_hook_dissociate("PRIVMSG", handle_privmsg);
     command_del("hello", cmd_hello);
+    timer_del("hello", timed_cmd_hello);
 }
 
 static void handle_privmsg(void *data, UNUSED void *udata)
@@ -32,4 +35,8 @@ static void cmd_hello(irc_event_t *event, UNUSED int parc, UNUSED char **parv)
     irc_privmsg(event->target, "Hello %s!", event->origin->nick);
 }
 
-
+static bool timed_cmd_hello(UNUSED void *argument)
+{
+    fprintf(stderr, "Oh look, this timer says hai thar!\n");
+    return true;
+}
