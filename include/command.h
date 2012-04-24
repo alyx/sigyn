2f3616ea72d77c693dac3de87270ca4e48d7efc8
Alyx <alyx@malkier.net>
2012-04-24 08:38:57 -0500

    This does not really work there.

--- include/command.h
+++ include/command.h
@@ -20,38 +20,9 @@ struct command_
 };
 
 extern void command_init(void);
-//extern command_t *command_find(const char *name);
+extern command_t *command_find(const char *name);
 extern void command_add(const char *name, void *function, const char *help, const char *syntax);
 extern void command_del(const char *name, void *function);
 
-/*
- * Routine Description:
- * This routine searches through the command list to locate
- * the specified command.
- *
- * Arguments:
- *     name - A string containing the name of the command to seach for.
- *
- * Return value:
- *     c - Returns a command_t object if the command is located,
- *         and returns NULL if it is not.
- *
- */
-
-static inline command_t * command_find(const char * name)
-{
-    command_t * c;
-    mowgli_node_t * n;
-
-    MOWGLI_ITER_FOREACH(n, commands.head)
-    {
-        c = (command_t *)n->data;
-
-        if (!strcasecmp(c->name, name))
-            return c;
-    }
-
-    return NULL;
-}
 
 #endif /* __SIGYN_COMMAND_H */
