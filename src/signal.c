af6e3cb775d865fdb4277a8ca4c68b57186fd16c
Alyx <alyx@malkier.net>
2012-04-25 15:18:25 -0700

    Cleaned signal rubbish a bit

--- src/signal.c
+++ src/signal.c
@@ -2,18 +2,10 @@
 
 #include <signal.h>
 
-static void signal_empty_handler(int signum)
-{
-    /* do nothing */
-}
-
-static void signal_hup_handler(int signum)
-{
-    /* XXX: I would almost think make this rehash the config,
+    /* XXX: I would almost think make to make SIGHUP this rehash the config,
      * but we really don't have a reason to rehash things, I think.
      * Thought needed here before development.
      */
-}
 
 static void signal_int_handler(int signum)
 {
@@ -28,11 +20,6 @@ static void signal_segv_handler(int signum)
     sigyn_fatal("Segmentation fault.");
 }
 
-static void signal_term_handler(int signum)
-{
-    /* Do something later */
-}
-
 static void signal_usr1_handler(int signum)
 {
     sigyn_fatal("Out of memory!");
@@ -40,9 +27,9 @@ static void signal_usr1_handler(int signum)
 
 void signals_init(void)
 {
-    mowgli_signal_install_handler(SIGHUP, signal_hup_handler);
+    mowgli_signal_install_handler(SIGHUP, SIG_IGN);
     mowgli_signal_install_handler(SIGINT, signal_int_handler);
-    mowgli_signal_install_handler(SIGTERM, signal_term_handler);
+    mowgli_signal_install_handler(SIGTERM, SIG_IGN);
 
     mowgli_signal_install_handler(SIGUSR1, signal_usr1_handler);
     mowgli_signal_install_handler(SIGSEGV, signal_segv_handler);
