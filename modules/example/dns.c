8176f78b3f38bbdca3b82b5f4a61f12c7494b2b5
Matthew <mattwb65@gmail.com>
2012-05-05 04:42:31 -0400

    Improve DNS command. The DNS command will now return all IP addresses associated with a host rather than just one.

--- modules/example/dns.c
+++ modules/example/dns.c
@@ -7,7 +7,7 @@ static void cmd_dns(const irc_event_t *event, int parc, char **parv);
 
 void _modinit(UNUSED module_t *m)
 {
-    command_add("dns", cmd_dns, "Resolves the IP address of a domain.", "<domain name>");
+    command_add("dns", cmd_dns, "Resolves the IP address(es) of a host.", "<host>");
 }
 
 void _moddeinit(UNUSED module_unload_intent_t intent)
@@ -19,8 +19,6 @@ static void cmd_dns(const irc_event_t *event, int parc, char **parv)
 {
     struct hostent *dns;
 
-    printf("parc size: %d\n", parc);
-
     if (parc < 1)
     {
         command_fail(CMD_NEEDSPARAM, event->origin, "dns");
@@ -31,9 +29,19 @@ static void cmd_dns(const irc_event_t *event, int parc, char **parv)
 
     if (dns == NULL)
     {
-        irc_privmsg(event->origin->nick, "DNS resolution failed.");
+        irc_privmsg(event->target, "DNS resolution failed.");
         return;
     }
-    else
-        irc_privmsg(event->target, "%s = %s", dns->h_name, inet_ntoa(*(struct in_addr *)dns->h_addr_list[0]));
+
+    char buf[BUFSIZE];
+    int i;
+    buf[0] = '\0';
+    while (dns->h_addr_list[i] != NULL) {
+        mowgli_strlcat(buf, " ", sizeof(buf));
+        mowgli_strlcat(buf, inet_ntoa(*(struct in_addr *)(dns->h_addr_list[i])), sizeof(buf));
+        i++;
+    }
+    irc_privmsg(event->target, "%s (%i):%s", dns->h_name, i, buf);
+
+
 }
