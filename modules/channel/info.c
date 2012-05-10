d23dc261f0659a2c1409babc0fcaea4f8df77dba
Alyx <alyx@malkier.net>
2012-05-09 18:21:38 -0700

    Move info to channel

--- modules/channel/info.c
+++ modules/channel/info.c
@@ -1,6 +1,6 @@
 #include "sigyn.h"
 
-DECLARE_MODULE("example/info", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddeinit,
+DECLARE_MODULE("channel/info", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddeinit,
         "1.0", "Matthew <mattwb65@gmail.com>");
 
 static void cmd_info(const irc_event_t *event, UNUSED int parc, UNUSED char **parv);
@@ -17,12 +17,14 @@ void _moddeinit(UNUSED module_unload_intent_t intent)
 
 static void cmd_info(const irc_event_t *event, UNUSED int parc, UNUSED char **parv)
 {
+    irc_channel_t * c;
+
     if (!ischannel(event->target))
         return;
-    channel_t *c = channel_find(event->target);
+    c = channel_find(event->target);
     if (c == NULL)
         irc_privmsg(event->target, "Unable to find channel.");
     else
-        irc_privmsg(event->target, "Name: %s Users: %i Topic: %s", c->name, c->usercount, c->topic);
+        irc_privmsg(event->target, "Name: %s Users: %i Topic: %s", c->name, c->nusers, c->topic);
 }
 
