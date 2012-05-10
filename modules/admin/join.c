150c35654f620a10307a8bb778b49682adf40995
Justin Crawford <Justasic@Gmail.com>
2012-05-10 16:04:51 -0700

    Just kidding, say bye to part.c, its function is being moved to join.c

--- modules/admin/join.c
+++ modules/admin/join.c
@@ -4,17 +4,39 @@ DECLARE_MODULE("admin/join", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddeinit,
         "1.0", "Justin Crawford <Justasic@gmail.com>");
 
 static void cmd_join(const irc_event_t *event, int parc, char **parv);
+static void cmd_part(const irc_event_t *event, int parc, char **parv);
 
 void _modinit(UNUSED module_t *m)
 {
     command_add("join", cmd_join, 1, AC_ADMIN, "Joins an IRC channel", "<channel> [<key>]");
+    command_add("part", cmd_part, 1, AC_ADMIN, "Parts an IRC channel", "<channel> [<reason>]");
 }
 
 void _moddeinit(UNUSED module_unload_intent_t intent)
 {
     command_del("join", cmd_join);
+    command_del("part", cmd_part);
 }
 
+static void cmd_part(const irc_event_t *event, int parc, char **parv)
+{
+  char *message = mowgli_alloc(BUFSIZE);
+  int i;
+  // Started at two so we don't get the command garbage, we only want the message!
+  for(i = 2; parv[i] != NULL; ++i)
+  {
+    strlcat(message, parv[i], BUFSIZE);
+    strlcat(message, " ", BUFSIZE);
+  }
+  // There's always a space at the end of the string, we don't want it!
+  size_t len = strlen(message);
+  if(isspace(message[len-1]))
+    message[len-1] = 0x00;
+  
+  irc_part(parv[1], message);
+  irc_notice(event->origin->nick, "Parted \2%s\2 successfully", parv[1]);
+  mowgli_free(message);
+}
 
 static void cmd_join(const irc_event_t *event, int parc, char **parv)
 {
