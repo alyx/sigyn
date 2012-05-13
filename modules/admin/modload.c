93f1837a365844b153ca9d69ab07069182843a6d
Justin Crawford <Justasic@Gmail.com>
2012-05-13 00:03:50 -0700

    Added modreload for reloading a module

--- modules/admin/modload.c
+++ modules/admin/modload.c
@@ -5,6 +5,7 @@ DECLARE_MODULE("admin/modload", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddeini
 
 static void cmd_modload(const irc_event_t *event, int parc, char **parv);
 static void cmd_modunload(const irc_event_t *event, int parc, char **parv);
+static void cmd_modreload(const irc_event_t *event, int parc, char **parv);
 static void cmd_modlist(const irc_event_t *event, int parc, char **parv);
 extern mowgli_list_t modules;
 
@@ -12,6 +13,7 @@ void _modinit(UNUSED module_t *m)
 {
     command_add("modload", cmd_modload, 1, AC_ADMIN, "Loads a module.", "<name>");
     command_add("modunload", cmd_modunload, 1, AC_ADMIN, "Unloads a module.", "<name>");
+    command_add("modreload", cmd_modreload, 1, AC_ADMIN, "Reloads a module.", "<name>");
     command_add("modlist", cmd_modlist, 0, AC_ADMIN, "Lists loaded modules.", NULL);
 }
 
@@ -60,6 +62,33 @@ static void cmd_modunload(const irc_event_t *event, int parc, char **parv)
     module_unload(mod, MODULE_UNLOAD_INTENT_PERM);
 
     irc_notice(event->origin->nick, "Module \2%s\2 successfully unloaded.", parv[1]);
+}
+
+static void cmd_modreload(const irc_event_t *event, int parc, char **parv)
+{
+    module_t *mod;
+
+    mod = module_find_published(parv[1]);
+    if (!mod)
+    {
+      irc_notice(event->origin->nick, "Module \2%s\2 is not loaded.", parv[1]);
+      return;
+    }
+    if (mod->can_unload != MODULE_UNLOAD_CAPABILITY_OK)
+    {
+      irc_notice(event->origin->nick, "Module \2%s\2 is a permanent module and cannot be unloaded.");
+      return;
+    }
+    
+    module_unload(mod, MODULE_UNLOAD_INTENT_RELOAD);
+
+    mod = module_load(parv[1]);
+    
+    if (mod == NULL)
+      irc_notice(event->origin->nick, "Module \2%s\2 failed to load.", parv[1]);
+    else
+      irc_notice(event->origin->nick, "Module \2%s\2 successfully reloaded.", parv[1]);
+    
 }
 
 static void cmd_modlist(const irc_event_t *event, int parc, char **parv)
