aefd3d9d561066dbcebad68af1375aa44a45ea2c
Matthew <mattwb65@gmail.com>
2012-05-15 06:13:07 +0200

    Use mowgli_timer

--- modules/example/hello.c
+++ modules/example/hello.c
@@ -5,20 +5,21 @@ DECLARE_MODULE("examples/hello", MODULE_UNLOAD_CAPABILITY_OK, _modinit, _moddein
 
 static void handle_privmsg(void *data, UNUSED void *udata);
 static void cmd_hello(const irc_event_t *event, UNUSED int parc, UNUSED char **parv);
-static bool timed_cmd_hello(UNUSED void *argument);
+static void timed_cmd_hello(UNUSED void *argument);
+static mowgli_eventloop_timer_t *hello_timer = NULL;
 
 void _modinit(UNUSED module_t *m)
 {
     mowgli_hook_associate("PRIVMSG", handle_privmsg, NULL);
     command_add("hello", cmd_hello, 0, AC_NONE, "says hello to you!", "<user>");
-    timer_add("hello", timed_cmd_hello, NULL, 10);
+    hello_timer = mowgli_timer_add(me.ev, "hello", timed_cmd_hello, NULL, 10);
 }
 
 void _moddeinit(UNUSED module_unload_intent_t intent)
 {
     mowgli_hook_dissociate("PRIVMSG", handle_privmsg);
     command_del("hello", cmd_hello);
-    timer_del("hello", timed_cmd_hello);
+    mowgli_timer_destroy(me.ev, hello_timer);
 }
 
 static void handle_privmsg(void *data, UNUSED void *udata)
@@ -35,8 +36,7 @@ static void cmd_hello(const irc_event_t *event, UNUSED int parc, UNUSED char **p
     command_reply(event->target, "Hello %s!", event->origin->nick);
 }
 
-static bool timed_cmd_hello(UNUSED void *argument)
+static void timed_cmd_hello(UNUSED void *argument)
 {
     logger(LOG_DEBUG, "Oh look, this timer says hai thar!");
-    return true;
 }
