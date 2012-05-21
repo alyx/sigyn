fbf06e0aad4fd4158509d0600d4b83a1ca0f3be6
Alyx <alyx@malkier.net>
2012-05-21 17:12:21 -0500

    mowgli this up, make sure we free our string clone

--- src/isupport.c
+++ src/isupport.c
@@ -4,7 +4,7 @@ static void parse_isupport(char * string)
 {
     char * save, * token, * tmp, * ptr;
 
-    tmp = strdup(string);
+    tmp = mowgli_strdup(string);
 
     token = strtok_r(tmp, " ", &save);
 
@@ -12,26 +12,28 @@ static void parse_isupport(char * string)
     {
         if (!strncmp(token, "CASEMAPPING", 11))
         {
-            tmp = token + 12;
+            ptr = token + 12;
             me.isupport.casemapping = strdup(tmp);
         }
         else if (!strncmp(token, "NICKLEN", 7))
         {
-            tmp = token + 8;
+            ptr = token + 8;
             me.isupport.len.nick = (size_t)strtol(tmp, (char **)NULL, 10);
         }
         else if (!strncmp(token, "CHANNELLEN", 10))
         {
-            tmp = token + 11;
+            ptr = token + 11;
             me.isupport.len.channel = (size_t)strtol(tmp, (char **)NULL, 10);
         }
         else if (!strncmp(token, "TOPICLEN", 8))
         {
-            tmp = token + 9;
+            ptr = token + 9;
             me.isupport.len.topic = (size_t)strtol(tmp, (char **)NULL, 10);
         }
 
         token = strtok_r(NULL, " ", &save);
     }
+
+    mowgli_free(tmp);
 }
 
