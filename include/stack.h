d6e5d53461dd26fbd0e491fff4ec515053d3b421
Alyx <alyx@malkier.net>
2012-05-29 17:27:13 -0500

    Derp

---
+++ include/stack.h
@@ -0,0 +1,26 @@
+#include "config.h"
+
+#ifdef ENABLE_STACK
+#   include <stdio.h>
+#   include <libunwind.h>
+static inline void sigyn_log_backtrace(void)
+{
+    char func[64];
+    unw_word_t    offset, pc;
+    unw_cursor_t  cursor;
+    unw_context_t context;
+
+    unw_getcontext(&context);
+    unw_init_local(&cursor, &context);
+
+    while (unw_step(&cursor) > 0)
+    {
+        unw_get_reg(&cursor, UNW_REG_IP, &pc);
+        func[0] = '\0';
+        unw_get_proc_name(&cursor, func, sizeof(func), &offset);
+        fprintf(stderr, "%p : (%s+0x%x) [%x]\n", (void *)pc, func, offset, pc);
+    }
+}
+#else
+static inline void sigyn_log_backtrace(void) { return; }
+#endif
